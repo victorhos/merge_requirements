b0347bc15422f0f71ed8bbd210197a1722d22532
Victor Hugo <victor.hos@gmail.com>
2015-07-17 20:16:24 -0300

    first commit

---
+++ File.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+
+class File(object):
+
+    def __init__():
+        file_merged = sys.args[0]
+        print(file_merged)
+
+    def main():
+        print(self.file_merged)
+
+
+if __name__ == '__main__':
+    main()
