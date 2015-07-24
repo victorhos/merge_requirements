9e89ced8d7edeefc9ba7c00aa8f6d9f71bf559b3
Victor Hugo <victor.hos@gmail.com>
2015-07-24 19:35:44 -0300

    requirements

---
+++ tests/tests_utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import unittest
+
+class TestUtils(unittest.TestCase):
+
+    def test_remove_comments(self):
+        pass
+
+if __name__ == '__main__':
+    main()
