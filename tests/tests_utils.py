36d86fb8ade8693fad2be3dc45cb0a360d4bfd3f
Victor Hugo <victor.hos@gmail.com>
2015-07-27 23:12:29 -0300

    imports tests

--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import mock
 import unittest
+from utils import remove_comments
 
 class TestUtils(unittest.TestCase):
 
     def test_remove_comments(self):
+
+
+
         pass
 
 if __name__ == '__main__':
