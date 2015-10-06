70370ea64cf3f1acb9c4dd9e5f105b32b71be7c6
Victor Hugo <victor.hos@gmail.com>
2015-10-06 19:35:39 -0300

    remove_comments ok

--- merge_requirements/tests/tests_utils.py
+++ merge_requirements/tests/tests_utils.py
@@ -11,24 +11,15 @@ class TestUtils(unittest.TestCase):
 
     def test_remove_comments(self):
 
-        text_file = '''
-#COMENTARIOS\n'CherryPy==3.2.4\nDjango==1.4.13\nIPTCInfo==1.9.5-6\nIon==0.6.4.2\n#COMENTARIO2\nJinja2==2.7\nMarkupSafe==0.18\nMySQL-python==1.2.3\nPIL==1.1.7-1\nPillow==2.1.0\nRoutes==2.0\nSQLAlchemy==0.5.8\nSouth==0.7.3\n
-        '''
+        text_file = '''#COMENTARIOS\nCherryPy==3.2.4\nDjango==1.4.13\nIPTCInfo==1.9.5-6\nIon==0.6.4.2\n#COMENTARIO2\nJinja2==2.7\nMarkupSafe==0.18\nMySQL-python==1.2.3\nPIL==1.1.7-1\nPillow==2.1.0\nRoutes==2.0\nSQLAlchemy==0.5.8\nSouth==0.7.3\n'''
 
-        expected_text_file = '''
-'CherryPy==3.2.4\nDjango==1.4.13\nIPTCInfo==1.9.5-6\nIon==0.6.4.2\nJinja2==2.7\nMarkupSafe==0.18\nMySQL-python==1.2.3\nPIL==1.1.7-1\nPillow==2.1.0\nRoutes==2.0\nSQLAlchemy==0.5.8\nSouth==0.7.3\n
-        '''
+        expected_text_file = '''CherryPy==3.2.4\nDjango==1.4.13\nIPTCInfo==1.9.5-6\nIon==0.6.4.2\nJinja2==2.7\nMarkupSafe==0.18\nMySQL-python==1.2.3\nPIL==1.1.7-1\nPillow==2.1.0\nRoutes==2.0\nSQLAlchemy==0.5.8\nSouth==0.7.3\n'''
 
-        with patch.object(
-            builtins,
-            'open',
-            mock_open(read_data=text_file)):
-
-            with open('foo') as handle:
-                self.assertEqual(
-                    remove_comments(handle.read()),
-                    expected_text_file
-                )
+        self.assertEqual(
+            remove_comments(text_file),
+            expected_text_file,
+            'okkkk'
+        )
 
 if __name__ == '__main__':
     unittest.main()
--- merge_requirements/utils.py
+++ merge_requirements/utils.py
@@ -5,7 +5,7 @@ import re
 
 def remove_comments(text):
 
-    regex = re.compile( '^#.*\n', re.M )
+    regex = re.compile( '#+[a-zA-Z0-9]*\\n', re.M | re.S)
 
     return regex.sub(
         '',
