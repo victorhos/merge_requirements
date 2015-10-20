4b26c324b17acc99a9bb6d94f8aca9661cfa96d9
Victor Hugo <victor.hos@gmail.com>
2015-10-20 10:59:59 -0200

    manage_file return dict

--- merge_requirements/manage_file.py
+++ merge_requirements/manage_file.py
@@ -13,9 +13,9 @@ class ManageFile(object):
 
     def __init__(self, fm, ff, sf):
 
-        self.file_merged = self.open_file(fm)
-        self.first_file = self.open_file(ff)
-        self.second_file = self.open_file(sf)
+        self.file_merged = remove_comments(self.open_file(fm))
+        self.first_file = self.modify_to_dict(ff)
+        self.second_file = self.modify_to_dict(sf)
 
     def open_file(self, file):
 
@@ -29,10 +29,18 @@ class ManageFile(object):
         except Exception as e:
             return logging.error(e)
 
+    def modify_to_dict(self, file):
+        text = remove_comments(self.open_file(file))
+        import ipdb; ipdb.set_trace()
+
+        return dict(
+            (l, v) for l, v in (item.split('==') for item in text.split('\n'))
+        )
+
     def see(self):
-        print(remove_comments(self.file_merged))
-        print(remove_comments(self.first_file))
-        print(remove_comments(self.second_file))
+        print(self.file_merged)
+        print(self.first_file)
+        print(self.second_file)
 
 class Merge(object):
 
